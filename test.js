68a6bf3c0327b0735de8546b1dfdc392ee75a38e
lgpersonal <ww7ta@sina.com>
2019-11-21 21:51:17 +0800

    删除多余js

--- test.js
+++
@@ -1,6 +0,0 @@
-textContains("搜一搜").findOne().parent().click();
-sleep(1000);
-text("小程序").findOne().click();
-sleep(1000);
-className("android.view.View").text("强国挑战答题答案查询").findOne().parent().parent().click();      
-sleep(1000);  
